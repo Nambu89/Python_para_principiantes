dec2de6bd493091014bfb9025785fce9d30ef7f1
Nambu89 <130612487+Nambu89@users.noreply.github.com>
2023-06-07 15:10:18 +0200

    Update ejercicio_unidad5.py
    He cambiado el código porque faltaban algunos elementos y he cambiado otros para que sea mas correcto.

--- TEMA 2/ejercicio_unidad5.py
+++ TEMA 2/ejercicio_unidad5.py
@@ -1,18 +1,21 @@
 smallest = None
-largest = -1
-value = None
-while value != 'fin':
-   value = input ()
-   try:
-     value = int(value)
-   except:
-       continue
-   if value > largest:
-       largest = value
-       continue
-   if smallest is None:
-       smallest = value
-   elif value < smallest:
-       smallest = value
-       continue
-print ('Mayor valor:', largest, 'Menor valor:', smallest)
+largest = None
+
+while True:
+    value = input("Ingrese un número o 'fin' para terminar: ")
+    if value.lower() == 'fin':
+        break
+    try:
+        value = int(value)
+    except ValueError:
+        print("Entrada no válida. Por favor, ingrese un número entero o 'fin' para terminar.")
+        continue
+    if largest is None or value > largest:
+        largest = value
+    if smallest is None or value < smallest:
+        smallest = value
+
+if smallest is not None and largest is not None:
+    print('Mayor valor: ', largest, 'Menor valor: ', smallest)
+else:
+    print('No se ingresó ningún número.')
